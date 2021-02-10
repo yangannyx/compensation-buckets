2711a85fc38d6141c4c292710ea91a8fbe6365b8
Anny Yang <yanga@ext.uber.com>
2021-02-10 11:43:44 -0800

    cleaning up staging

--- src/components/tooltip/tooltip.test.js
+++
@@ -1,9 +0,0 @@
-import { shallow } from 'enzyme';
-import { Tooltip } from './';
-
-describe('Tooltip', () => {
-  it('should mount', () => {
-    const wrapper = shallow(<Tooltip title="Sample Tooltip" info="yoyoyo" link="someLink" />);
-    expect(wrapper).toMatchSnapshot();
-  });
-});
